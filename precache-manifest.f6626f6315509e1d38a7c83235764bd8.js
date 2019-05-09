c8e32ba831250e7ad1a600effad29f58e22e8afa
Rishabh176 <rishabhemail8@gmail.com>
2019-05-10 01:33:39 +0530

    Updates

---
+++ precache-manifest.f6626f6315509e1d38a7c83235764bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ea8d525f1ef8de7e669fb132619a3a",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "3d7ffc838e8a81c96aa5",
+    "url": "/to-do-list/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a970eab7c46dc1b60d5e",
+    "url": "/to-do-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3d7ffc838e8a81c96aa5",
+    "url": "/to-do-list/static/js/2.a8320557.chunk.js"
+  },
+  {
+    "revision": "a970eab7c46dc1b60d5e",
+    "url": "/to-do-list/static/js/main.cdf4612b.chunk.js"
+  },
+  {
+    "revision": "15bb3101340a9b514097",
+    "url": "/to-do-list/static/js/runtime~main.9900d293.js"
+  }
+]);
